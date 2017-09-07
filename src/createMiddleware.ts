8edb32a1753917a7f3bf4b8585442f460dbef9c0
adz5a <antoine.d.zimmermann@gmail.com>
2017-09-07 15:13:47 +0200

    fix signature for run function

--- src/createMiddleware.ts
+++ src/createMiddleware.ts
@@ -12,7 +12,7 @@ import {
 export type Action$ = Stream<Action>;
 
 export interface Run {
-    <S> ( action$: Stream<Action>, api?: MiddlewareAPI<S> ): Stream<Action>
+    <S> ( action$: Stream<Action>, api: MiddlewareAPI<S> ): Stream<Action>
     <S> ( action$: Stream<Action> ): Stream<Action>
 }
 export function createMiddleware ( run: Run, name: string | null = null ): Middleware {
